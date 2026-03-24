ef70db7492a641383fed4060ec9198d14bf03ceb
Андрей Подоляко <andrey.podolyako@factory.vocord.ru>
2026-03-24 23:32:13 +0300

    q

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/vk-its/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
